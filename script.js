a07b065863819c7a5bfb2d4572d7f8b7fd22804a
KhanaghaNoori <kn.khannoori@gmail.com>
2024-10-28 19:45:29 +0100

    project up

---
+++ script.js
@@ -0,0 +1,13 @@
+// const taskInput = document.getElementById("taskInput");
+// const taskButton = document.getElementById("addTaskButton");
+// const taskList = document.getElementById("taskList");
+
+// taskButton.addEventListener("click", function (){
+//     let taskText = taskInput.value;
+
+//     let listItem= document.createElement("li");
+//     listItem.textContent = taskText;
+
+//     taskList.appendChild(listItem);
+// });
+
